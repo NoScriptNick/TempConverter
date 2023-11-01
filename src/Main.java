5f71ba396ce1cf86abb15b0a17338aa74eb3509b
613668 <beres.613668@swl.k12.oh.us>
2023-11-01 09:12:59 -0400

    dfgdf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //variables
+        double c;
+        double f;
+        boolean done = false;
+        String stop;
+
+        do {
+            System.out.println("Enter temperatures in celsius. Enter N to stop: ");
+            if (scan.hasNextDouble()) {
+                c = scan.nextDouble();
+                scan.nextLine();
+                f = (c*9/5) + 32;
+                System.out.println(c + " degrees in celsius is " + f + " degrees in fahrenheit.");
+            } else {
+                stop = scan.nextLine();
+                if (stop.equalsIgnoreCase("N")) {
+                    done = true;
+                } else {
+                    System.out.println("Try again pal");
+                }
+            }
+        } while (!done);
     }
 }
